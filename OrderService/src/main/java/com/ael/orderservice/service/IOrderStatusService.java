5eb1821eb8a8a8e43a2aa7c7716a0da8daa2ca53
Tarık Hamarat <trkhamarat@gmail.com>
2025-09-18 20:09:13 +0300

    .

--- OrderService/src/main/java/com/ael/orderservice/service/IOrderStatusService.java
+++
@@ -1,7 +0,0 @@
-package com.ael.orderservice.service;
-
-import com.ael.orderservice.model.OrderStatus;
-
-public interface IOrderStatusService {
-    OrderStatus getOrderStatusByOrderStatusId(Integer orderStatusId);
-}
